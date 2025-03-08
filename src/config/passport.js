5f4681b1fa07ae0fa9990c412a92b92597776aac
devlow03 <nth4356@gmnail.com>
2025-03-08 15:52:15 +0700

    feat: add env example

--- src/config/passport.js
+++ src/config/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4040/api/auth/google/callback",
+      callbackURL: process.env.GOOGLE_REDIRECT_URI,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
