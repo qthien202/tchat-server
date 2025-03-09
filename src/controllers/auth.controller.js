3cb97e9e5c61387ff5fd5c8e4c608ea70d332d10
devlow03 <nth4356@gmnail.com>
2025-03-09 15:18:52 +0700

    feat: add get user controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -42,3 +42,22 @@ exports.googleAuthCallback = (req, res, next) => {
     }
   )(req, res, next);
 };
+
+const User = require("../models/User");
+
+exports.getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id); // Lấy ID từ middleware
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json({
+      userId: user._id,
+      name: user.name,
+      email: user.email,
+      picture: user.picture,
+    });
+  } catch (error) {
+    console.error("Error fetching user info:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
