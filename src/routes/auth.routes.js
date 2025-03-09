3efede6e2152e11672b28e582ac31837279952ae
devlow03 <nth4356@gmnail.com>
2025-03-09 15:19:52 +0700

    feat: add get user info route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const authController = require("../controllers/auth.controller");
 
 const router = express.Router();
+const authMiddleware = require("../middlewares/auth.middleware");
 
 router.get("/google", authController.googleAuth);
 router.get("/google/callback", authController.googleAuthCallback);
-
+router.get("/user", authMiddleware, authController.getUserInfo);
 module.exports = router;
