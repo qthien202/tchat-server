d98ac24c0270ecd665d0f5b7052f76f6518c8d1d
devlow03 <nth4356@gmnail.com>
2025-03-09 14:58:07 +0700

    fix: redirect app flutter

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,24 +5,40 @@ exports.googleAuth = passport.authenticate("google", {
   scope: ["profile", "email"],
 });
 
+// exports.googleAuthCallback = (req, res, next) => {
+//   passport.authenticate(
+//     "google",
+//     { failureRedirect: "/login" },
+//     (err, data) => {
+//       if (err || !data)
+//         return res.status(401).json({ message: "Google login failed" });
+
+//       const { user } = data;
+//       const token = generateToken(user); // Tạo JWT token
+
+//       res.json({
+//         userId: user._id,
+//         name: user.name,
+//         email: user.email,
+//         picture: user.picture,
+//         accessToken: token, // Trả về JWT
+//       });
+//     }
+//   )(req, res, next);
+// };
+
 exports.googleAuthCallback = (req, res, next) => {
   passport.authenticate(
     "google",
     { failureRedirect: "/login" },
-    (err, data) => {
-      if (err || !data)
-        return res.status(401).json({ message: "Google login failed" });
+    (err, user) => {
+      if (err || !user)
+        return res.redirect("http://localhost:3001/auth_failed");
 
-      const { user } = data;
       const token = generateToken(user); // Tạo JWT token
 
-      res.json({
-        userId: user._id,
-        name: user.name,
-        email: user.email,
-        picture: user.picture,
-        accessToken: token, // Trả về JWT
-      });
+      // Redirect về Flutter với token trong URL
+      res.redirect(`http://localhost:3001/auth_success?token=${token}`);
     }
   )(req, res, next);
 };
